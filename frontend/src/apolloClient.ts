517c42298736815382ed21cc8c549a6161b26b7e
Luke Zhang <lukezhang1998@gmail.com>
2021-03-15 09:57:13 -0400

    Revert "@luke/graphql env var"
    This reverts commit c8311f95a62bfcc05ab51344a550edaf4ae4ed4c.

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -1,10 +1,8 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    // https://api.codecollab.me. Make uri an environment variable
-    // uri: "http://localhost:4000/graphql",
-    uri: process.env.REACT_APP_GRAPHQL_SERVER,
-    cache: new InMemoryCache(),
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache()
 });
 
 export default client;
