1c4ee9447273ff7fc07df4931fd7af89ebcd8870
Luke Zhang <lukezhang1998@gmail.com>
2021-03-16 16:44:48 -0400

    @luke/https

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://codecollab.me/graphql",
+    uri: "https://codecollab.me/graphql",
     cache: new InMemoryCache(),
 });
 
