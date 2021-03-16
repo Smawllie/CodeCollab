d1ad1186f353ee6cccadccad4c44e78fba8cb30b
Luke Zhang <lukezhang1998@gmail.com>
2021-03-16 17:17:24 -0400

    @luke/env variable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,43 @@
-import { ApolloProvider } from '@apollo/client';
-import React from 'react';
-import { BrowserRouter, Route, Switch, RouteComponentProps } from 'react-router-dom';
-import routes from './config/route';
-import client from './apolloClient';
-
+import { ApolloProvider } from "@apollo/client";
+import React from "react";
+import {
+    BrowserRouter,
+    Route,
+    Switch,
+    RouteComponentProps,
+} from "react-router-dom";
+import routes from "./config/route";
+import client from "./apolloClient";
 
 const App: React.FunctionComponent<{}> = (props) => {
-	return (<div>
-        <ApolloProvider client={client}>
-        <BrowserRouter>
-            <Switch>
-                {routes.map((route,index)=>{
-                  return (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        exact={route.exact}
-                        render={(props:RouteComponentProps<any>)=>(
-                            <route.component
-                            name={route.name} 
-                            {...props} 
-                            {...route.props}/>
-                        )}
-                    />
-                  );
-                })}
-            </Switch>
-        </BrowserRouter>
-        </ApolloProvider>
-
-    </div>);
+    return (
+        <div>
+            <ApolloProvider client={client}>
+                <BrowserRouter>
+                    <Switch>
+                        {routes.map((route, index) => {
+                            return (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    exact={route.exact}
+                                    render={(
+                                        props: RouteComponentProps<any>
+                                    ) => (
+                                        <route.component
+                                            name={route.name}
+                                            {...props}
+                                            {...route.props}
+                                        />
+                                    )}
+                                />
+                            );
+                        })}
+                    </Switch>
+                </BrowserRouter>
+            </ApolloProvider>
+        </div>
+    );
 };
 
 export default App;
--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://codecollab.me/graphql",
+    // uri: "https://codecollab.me/graphql",
+    uri: process.env.REACT_APP_APOLLO_SERVER_URI,
     cache: new InMemoryCache(),
 });
 
