2ac9ae9b18b133d48b295c41a1483f1d82a4d5c3
Laphonso Reyes <laphonso.reyes@gmail.com>
2021-03-19 14:58:57 -0400

    check if user or collaborator when writing

--- backend/src/modules/file/writeFile.resolver.ts
+++ backend/src/modules/file/writeFile.resolver.ts
@@ -1,28 +1,38 @@
 import { Arg, Authorized, Ctx, Mutation, Resolver } from "type-graphql";
+import { AuthenticationError } from "apollo-server-errors";
+import { isDocument } from "@typegoose/typegoose";
 
 import { File, FileModel } from "../../entities/file.entity";
-import { ProjectModel } from "../../entities/project.entity";
 import { Context } from "../../context";
-import { AuthenticationError } from "apollo-server-errors";
 import { WriteFileInput } from "./input/writeFile.input";
 
 @Resolver()
 export class WriteFileResolver {
     @Mutation(() => File)
     @Authorized()
-    // TODO: Add writeFile input object
     async writeFile(
         @Arg("file") { fileId, content }: WriteFileInput,
         @Ctx() context: Context
     ) {
-        let file = await FileModel.findById(fileId);
+        let file = await FileModel.findById(fileId)
+            .populate({ path: "project", model: "Project" })
+            .exec();
 
         if (!file) throw new Error(`File with id: ${fileId} does not exist`);
 
-        let project = await ProjectModel.findById(file.project);
+        let userId = context.req.session.userId;
 
-        if (project!.owner!.toString() !== context.req.session.userId)
-            throw new AuthenticationError("Access Denied");
+        if (isDocument(file.project)) {
+            let isOwner = file.project!.owner!.toString() === userId;
+            let isCollaborator = file.project.collaborators.some(
+                (userId: any) => userId.toString() === userId
+            );
+
+            if (!isOwner && !isCollaborator)
+                throw new AuthenticationError(
+                    `You must be a collaborator or owner of this project to write to this file`
+                );
+        }
 
         file.content = content;
         await file.save();
