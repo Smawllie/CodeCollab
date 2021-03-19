dff64328abe587c29b951131e2ad304b09a2bee8
Laphonso Reyes <laphonso.reyes@gmail.com>
2021-03-19 14:43:06 -0400

    Check permissions to project and if there exists a item in the directory

--- backend/src/modules/file/addFile.resolver.ts
+++ backend/src/modules/file/addFile.resolver.ts
@@ -1,18 +1,52 @@
-import { Arg, Authorized, Mutation, Resolver } from "type-graphql";
+import { Arg, Authorized, Ctx, Mutation, Resolver } from "type-graphql";
+import { AuthenticationError } from "apollo-server-express";
+import { isDocument, isDocumentArray } from "@typegoose/typegoose";
 
 import { File, FileModel } from "../../entities/file.entity";
 import { DirectoryModel } from "../../entities/directory.entity";
 import { AddFileInput } from "./input/addFile.input";
+import { Context } from "../../context";
 
 @Resolver()
 export class AddFileResolver {
     @Mutation(() => File)
     @Authorized()
-    async addFile(@Arg("file") { name, parentId, content }: AddFileInput) {
-        let parent = await DirectoryModel.findById(parentId);
+    async addFile(
+        @Arg("file") { name, parentId, content }: AddFileInput,
+        @Ctx() context: Context
+    ) {
+        let parent = await DirectoryModel.findById(parentId)
+            .populate({ path: "project", model: "Project" })
+            .populate({ path: "contents", model: "Resource" })
+            .exec();
 
         if (!parent)
             throw new Error(`Directory with id: ${parentId} does not exist`);
+
+        let userId = context.req.session.userId;
+
+        if (isDocument(parent.project)) {
+            let isOwner = parent.project!.owner!.toString() === userId;
+            let isCollaborator = parent.project.collaborators.some(
+                (userId: any) => userId.toString() === userId
+            );
+
+            if (!isOwner && !isCollaborator)
+                throw new AuthenticationError(
+                    `You must be a collaborator or owner of this project to add a new file`
+                );
+        }
+
+        if (isDocumentArray(parent.contents)) {
+            let resourceNameExists = parent.contents.some(
+                (resource) => resource.name === name
+            );
+
+            if (resourceNameExists)
+                throw new Error(
+                    `This directory already contains an item with name: ${name}`
+                );
+        }
 
         let file = new FileModel({
             name,
@@ -22,8 +56,7 @@ export class AddFileResolver {
         });
 
         parent.contents.push(file._id);
-        await parent.save();
-        await file.save();
+        await Promise.all([parent.save(), file.save()]);
 
         return file;
     }
