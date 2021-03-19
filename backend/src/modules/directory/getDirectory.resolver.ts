2288744acf150ed88b663b2eb6e6fb60c7f9da3d
Laphonso Reyes <laphonso.reyes@gmail.com>
2021-03-19 14:30:11 -0400

    use await again

--- backend/src/modules/directory/getDirectory.resolver.ts
+++ backend/src/modules/directory/getDirectory.resolver.ts
@@ -7,15 +7,11 @@ export class GetDirectoryResolver {
     @Query(() => Directory)
     @Authorized()
     async getDirectoryById(@Arg("id") id: String) {
-        return new Promise((res, _) =>
-            DirectoryModel.findById(id, (err: any, directory: Directory) => {
-                if (err) throw new Error();
+        let directory = await DirectoryModel.findById(id).exec();
 
-                if (!directory)
-                    throw new Error(`Directory with id: ${id} does not exist`);
+        if (!directory)
+            throw new Error(`Directory with id: ${id} does not exist`);
 
-                res(directory);
-            })
-        );
+        return directory;
     }
 }
