51f42d41aade197f559208af38cbd7ac31bb84a9
Laphonso Reyes <laphonso.reyes@gmail.com>
2021-03-19 13:48:37 -0400

    save project if the user does not have any projects with the same name

--- backend/src/modules/project/createProject.resolver.ts
+++ backend/src/modules/project/createProject.resolver.ts
@@ -1,4 +1,5 @@
 import { Arg, Authorized, Ctx, Mutation, Resolver } from "type-graphql";
+import { isDocumentArray } from "@typegoose/typegoose";
 
 import { Context } from "../../context";
 import { Project, ProjectModel } from "../../entities/project.entity";
@@ -15,12 +16,25 @@ export class CreateProjectResolver {
         @Ctx() context: Context
     ): Promise<Project> {
         // Check if user exists
-        let user = await UserModel.findById(context.req.session.userId);
+        let user = await UserModel.findById(context.req.session.userId)
+            .populate({ path: "createdProjects", model: "Project" })
+            .exec();
 
         if (!user)
             throw new Error(
                 `User ID ${context.req.session.userId} does not exist`
             );
+
+        if (isDocumentArray(user.createdProjects)) {
+            let projectNameExists = user.createdProjects.some(
+                (project) => project.name === name
+            );
+
+            if (projectNameExists)
+                throw new Error(
+                    `You already have a project with name: ${name}`
+                );
+        }
 
         // Create new project and its root directory
         let project = new ProjectModel({
@@ -40,9 +54,7 @@ export class CreateProjectResolver {
         user.createdProjects.push(project._id);
 
         // Save it all to the database
-        await root.save();
-        await project.save();
-        await user.save();
+        await Promise.all([root.save(), project.save(), user.save()]);
 
         return project;
     }
