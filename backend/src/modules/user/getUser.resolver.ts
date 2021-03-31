4cfb153e677796ddf332908e053f99d661fc53ea
Laphonso Reyes <laphonso.reyes@gmail.com>
2021-03-31 15:03:02 -0400

    Add get current logged in user query

--- backend/src/modules/user/getUser.resolver.ts
+++ backend/src/modules/user/getUser.resolver.ts
@@ -1,12 +1,25 @@
-import { Arg, Authorized, Query, Resolver } from "type-graphql";
+import { Arg, Authorized, Ctx, Query, Resolver } from "type-graphql";
 
 import { User, UserModel } from "../../entities/user.entity";
+import { Context } from "../../context";
 
 @Resolver(() => User)
 export class GetUserResolver {
     @Query(() => User)
     @Authorized()
     async getUserById(@Arg("id") id: String) {
+        let user = await UserModel.findById(id).exec();
+
+        if (!user) throw new Error(`User with id: ${id} does not exist`);
+
+        return user;
+    }
+
+    @Query(() => User)
+    @Authorized()
+    async getCurrentUser(@Ctx() context: Context) {
+        let id = context.req.session.userId;
+
         let user = await UserModel.findById(id).exec();
 
         if (!user) throw new Error(`User with id: ${id} does not exist`);
